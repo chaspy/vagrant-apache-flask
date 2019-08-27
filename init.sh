731a051ae157ec557fe743dddeab54385bd1b3e2
kondo takeshi <chaspy@users.noreply.github.com>
2019-08-27 19:23:37 +0900

    Install python from epel

--- init.sh
+++ init.sh
@@ -4,10 +4,19 @@ sudo yum install -y httpd
 sudo yum install -y mod_wsgi
 
 # Locale
-grep 'LC_ALL="ja_JP.utf8"' ~/.bash_profile || echo 'LC_ALL="ja_JP.utf8"' >> ~/.bash_profile
+grep 'LC_ALL="ja_JP.utf8"' /home/vagrant/.bash_profile || echo 'LC_ALL="ja_JP.utf8"' >> /home/vagrant/.bash_profile
 export LC_ALL="ja_JP.utf8"
 
 # Python
+yum install -y epel-release
+yum-config-manager --disable epel
+yum install -y --enablerepo="epel" python36
+
+mkdir -pv /home/vagrant/.venv
+python36 -mvenv /home/vagrant/.venv
+
+grep 'source /home/vagrant/.venv/bin/activate' ~/.bash_profile || echo 'source /home/vagrant/.venv/bin/activate' >> /home/vagrant/.bash_profile
+source /home/vagrant/.venv/bin/activate
 
 # flask
 pip install --upgrade pip
