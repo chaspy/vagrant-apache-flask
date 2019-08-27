8c3bc30523c2016a4a678b73f22b0ef3268d50d3
kondo takeshi <chaspy@users.noreply.github.com>
2019-08-27 16:14:58 +0900

    Use Python 3.6 via vagrant ssh

--- init.sh
+++ init.sh
@@ -7,3 +7,4 @@ sudo yum install -y centos-release-scl
 sudo yum install -y rh-python36
 # ref: https://linuxize.com/post/how-to-install-python-3-on-centos-7/#installing-python-3-on-centos-7
 grep 'scl enable rh-python36 bash' ~/.bash_profile || echo 'scl enable rh-python36 bash' >> ~/.bash_profile
+scl enable rh-python36 bash
