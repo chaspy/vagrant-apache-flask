79ce97b7fa03b7e74340734ea3ee0fe0f3985fb2
kondo takeshi <chaspy@users.noreply.github.com>
2019-08-27 16:15:22 +0900

    Install flask

--- init.sh
+++ init.sh
@@ -8,3 +8,7 @@ sudo yum install -y rh-python36
 # ref: https://linuxize.com/post/how-to-install-python-3-on-centos-7/#installing-python-3-on-centos-7
 grep 'scl enable rh-python36 bash' ~/.bash_profile || echo 'scl enable rh-python36 bash' >> ~/.bash_profile
 scl enable rh-python36 bash
+
+# flask
+pip install --upgrade pip
+pip3 install flask
