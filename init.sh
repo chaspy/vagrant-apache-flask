5cf5a1bd67b9f5d7761da39ae027f3bf249c55a3
kondo takeshi <chaspy@users.noreply.github.com>
2019-08-27 17:51:19 +0900

    Stop using rh-python36

--- init.sh
+++ init.sh
@@ -8,11 +8,6 @@ grep 'LC_ALL="ja_JP.utf8"' ~/.bash_profile || echo 'LC_ALL="ja_JP.utf8"' >> ~/.b
 export LC_ALL="ja_JP.utf8"
 
 # Python
-sudo yum install -y centos-release-scl
-sudo yum install -y rh-python36
-# ref: https://linuxize.com/post/how-to-install-python-3-on-centos-7/#installing-python-3-on-centos-7
-grep 'scl enable rh-python36 bash' ~/.bash_profile || echo 'scl enable rh-python36 bash' >> ~/.bash_profile
-scl enable rh-python36 bash
 
 # flask
 pip install --upgrade pip
