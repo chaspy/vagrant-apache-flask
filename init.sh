b932389bd827473c07ed71bf119ea066f81386d3
kondo takeshi <chaspy@users.noreply.github.com>
2019-08-27 16:07:41 +0900

    Use python 3.6 by default

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ sudo yum install -y httpd
 # Python
 sudo yum install -y centos-release-scl
 sudo yum install -y rh-python36
+# ref: https://linuxize.com/post/how-to-install-python-3-on-centos-7/#installing-python-3-on-centos-7
+grep 'scl enable rh-python36 bash' ~/.bash_profile || echo 'scl enable rh-python36 bash' >> ~/.bash_profile
